368ab91bb3c840e467740712b93542963a38aa31
Carmine Calabrese <carmine.calabrese99@gmail.com>
2025-10-02 12:37:49 +0200

    Enable Playwright E2E tests and add version indicator to header
    - Uncommented Playwright test steps in CI/CD workflow
    - Added Docker build/run for E2E testing environment
    - Installed Playwright dependencies in GitHub Actions
    - Added v2.1.0 version indicator to Header component
    - Frontend change will trigger complete testing pipeline

--- MuSe-Remix-Plugin/src/components/Header.jsx
+++ MuSe-Remix-Plugin/src/components/Header.jsx
@@ -4,6 +4,9 @@ const Header = () => {
       <div className="header-content">
         <div>
           <img className="app-icon" src="logo.png" alt="MuSe Logo" />
+          <span className="version-indicator" style={{ fontSize: '0.8em', color: '#666', marginLeft: '8px' }}>
+            v2.1.0
+          </span>
         </div>
       </div>
     </header>
